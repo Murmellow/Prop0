368d1689b6551728d60ccead423176cf4c58b921
Murmellow <max.lund.jonsson@gmail.com>
2016-11-14 16:31:08 +0100

    Signed-off-by: Murmellow <max.lund.jonsson@gmail.com>

--- prop0/src/prop/assignment0/Parser.java
+++ prop0/src/prop/assignment0/Parser.java
@@ -1,88 +1,109 @@
 package prop.assignment0;
 
 import java.io.IOException;
+
 //Beatrice
 class Parser {
-    Tokenizer t = null;
+	Tokenizer t = null;
 
-    void open(String fileName) throws IOException, TokenizerException {
-	t = new Tokenizer();			
-	t.open(fileName);
-	t.moveNext();
-    }
-
-    	public INode parse() throws IOException, TokenizerException, ParserException {
-	    if (t == null)
-		throw new IOException("No open file.");
-
-	    return new TextNode(t);	    
+	void open(String fileName) throws IOException, TokenizerException {
+		t = new Tokenizer();
+		t.open(fileName);
+		t.moveNext();
 	}
 
-    public void close() throws IOException {
-	if (t != null)
-	    t.close();
-    }
-    
-    class TextNode implements INode{
-	SentenceNode s = null;
-	TextNode tx = null;
+	public INode parse() throws IOException, TokenizerException, ParserException {
+		if (t == null)
+			throw new IOException("No open file.");
 
-	public TextNode(Tokenizer t) {
-	    s = new SentenceNode(t);
-	    if (t.current().token() != Token.EOF)
-		tx = new TextNode(t);
+		return new AssignNode(t);
 	}
-    }
 
-    class SentenceNode implements INode{
-	NounphraseNode n = null;
-	VerbphraseNode v = null;
-
-	public SentenceNode(Tokenizer t) {
-	    n = new NounphraseNode(t);
-	    v = new VerbphraseNode(t);
+	public void close() throws IOException {
+		if (t != null)
+			t.close();
 	}
-    }
 
-    class NounphraseNode implements INode {
-	DeterminerNode d = null;
-	NounNode n = null;
-	public NounphraseNode(Tokenizer t) {
-	    d = new DeterminerNode(t);
-	    n = new NounNode(t);
+	class AssignNode implements INode {
+		ident=null;
+		assign_op= "=";
+		ExprNode expr = null;
+		SEMICOLON = ";";
+		AssignNode assign = null;
+
+		public AssignNode(Tokenizer t) {
+			expr = new ExprNode(t);
+			if (t.current().token() != Token.EOF)
+				assign = new AssignNode(t);
+		}
+
+		@Override
+		public Object evaluate(Object[] args) throws Exception {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public void buildString(StringBuilder builder, int tabs) {
+			// TODO Auto-generated method stub
+			
+		}
 	}
-    }
 
-    class VerbphraseNode implements INode {
-	VerbNode v = null;
-	NounphraseNode n = null;
+	class ExprNode implements INode {
+		TermNode term = null;
+		ADD_OP = "+";
+		ExprNode expr = null;
 
-	public VerbphraseNode(Tokenizer t) {
-	    v = new VerbNode(t);
-	    n = new NounphraseNode(t);
+		public ExprNode(Tokenizer t) {
+			term = new TermNode(t);
+			expr = new ExprNode(t);
+		}
 	}
-    }
 
-    class DeterminerNode implements INode {
-	Lexeme l = null;
-	public DeterminerNode(Tokenizer t) {
-	    // get next token and check that it's a determiner
-	}
-    }
+	class TermNode implements INode {
+		FactorNode factor = null;
+		MULT_OP = "*";
+		TermNode term = null;
 
-    class NounNode implements INode {
-	Lexeme l = null;
-	public NounNode(Tokenizer t) {
-	    // get next token and check that it's a Noun
+		public TermNode(Tokenizer t) {
+			factor = new FactorNode(t);
+			term = new TermNode(t);
+		}
 	}
-    }
 
-    class VerbNode implements INode {
-	Lexeme l = null;
-	public VerbNode(Tokenizer t) {
-	    // get next token and check that it's a Verb
+	class FactorNode implements INode {
+		INT_LIT = null;
+		TermNode term = null;
+
+		public FactorNode(Tokenizer t) {
+			
+			term = new TermNode(t);
+		}
 	}
-    }
+
+	class DeterminerNode implements INode {
+		Lexeme l = null;
+
+		public DeterminerNode(Tokenizer t) {
+			// get next token and check that it's a determiner
+		}
+	}
+
+	class NounNode implements INode {
+		Lexeme l = null;
+
+		public NounNode(Tokenizer t) {
+			// get next token and check that it's a Noun
+		}
+	}
+
+	class VerbNode implements INode {
+		Lexeme l = null;
+
+		public VerbNode(Tokenizer t) {
+			// get next token and check that it's a Verb
+		}
+	}
 
 }
-
