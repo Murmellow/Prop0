7f898eaa03b53a5c8e859cbc0f166b92d2782074
Murmellow <max.lund.jonsson@gmail.com>
2016-11-14 15:24:59 +0100

    Signed-off-by: Murmellow <max.lund.jonsson@gmail.com>

---
+++ prop0/src/prop/assignment0/Tokenizer.java
@@ -0,0 +1,31 @@
+package prop.assignment0;
+
+import java.io.IOException;
+
+public class Tokenizer implements ITokenizer {
+
+	@Override
+	public void open(String fileName) throws IOException, TokenizerException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Lexeme current() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void moveNext() throws IOException, TokenizerException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void close() throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
