1c57c9f3344df3561cc86d4ecc2858195fedf18b
Murmellow <max.lund.jonsson@gmail.com>
2016-11-14 15:51:46 +0100

    Signed-off-by: Murmellow <max.lund.jonsson@gmail.com>

--- prop0/src/prop/assignment0/Parser.java
+++ prop0/src/prop/assignment0/Parser.java
@@ -1,25 +1,88 @@
 package prop.assignment0;
 
 import java.io.IOException;
+//Beatrice
+class Parser {
+    Tokenizer t = null;
 
-public class Parser implements IParser {
+    void open(String fileName) throws IOException, TokenizerException {
+	t = new Tokenizer();			
+	t.open(fileName);
+	t.moveNext();
+    }
 
-	@Override
-	public void open(String fileName) throws IOException, TokenizerException {
-		// TODO Auto-generated method stub
+    	public INode parse() throws IOException, TokenizerException, ParserException {
+	    if (t == null)
+		throw new IOException("No open file.");
 
+	    return new TextNode(t);	    
 	}
 
-	@Override
-	public INode parse() throws IOException, TokenizerException, ParserException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public void close() throws IOException {
+	if (t != null)
+	    t.close();
+    }
+    
+    class TextNode implements INode{
+	SentenceNode s = null;
+	TextNode tx = null;
 
-	@Override
-	public void close() throws IOException {
-		// TODO Auto-generated method stub
-
+	public TextNode(Tokenizer t) {
+	    s = new SentenceNode(t);
+	    if (t.current().token() != Token.EOF)
+		tx = new TextNode(t);
 	}
+    }
+
+    class SentenceNode implements INode{
+	NounphraseNode n = null;
+	VerbphraseNode v = null;
+
+	public SentenceNode(Tokenizer t) {
+	    n = new NounphraseNode(t);
+	    v = new VerbphraseNode(t);
+	}
+    }
+
+    class NounphraseNode implements INode {
+	DeterminerNode d = null;
+	NounNode n = null;
+	public NounphraseNode(Tokenizer t) {
+	    d = new DeterminerNode(t);
+	    n = new NounNode(t);
+	}
+    }
+
+    class VerbphraseNode implements INode {
+	VerbNode v = null;
+	NounphraseNode n = null;
+
+	public VerbphraseNode(Tokenizer t) {
+	    v = new VerbNode(t);
+	    n = new NounphraseNode(t);
+	}
+    }
+
+    class DeterminerNode implements INode {
+	Lexeme l = null;
+	public DeterminerNode(Tokenizer t) {
+	    // get next token and check that it's a determiner
+	}
+    }
+
+    class NounNode implements INode {
+	Lexeme l = null;
+	public NounNode(Tokenizer t) {
+	    // get next token and check that it's a Noun
+	}
+    }
+
+    class VerbNode implements INode {
+	Lexeme l = null;
+	public VerbNode(Tokenizer t) {
+	    // get next token and check that it's a Verb
+	}
+    }
 
 }
+
--- prop0/src/prop/assignment0/Tokenizer.java
+++ prop0/src/prop/assignment0/Tokenizer.java
@@ -1,31 +1,114 @@
 package prop.assignment0;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
+//Beatrice
+public class Tokenizer {
 
-public class Tokenizer implements ITokenizer {
+    private static Map<Character, Token> symbols = null;
 
-	@Override
-	public void open(String fileName) throws IOException, TokenizerException {
-		// TODO Auto-generated method stub
+    private Scanner scanner = null;
+    private Lexeme current = null;
+    private Lexeme next = null;
 
+    public Tokenizer() {
+	symbols = new HashMap<Character, Token>();
+	symbols.put('.', Token.STOP);
+	symbols.put(Scanner.EOF, Token.EOF);
+    }
+
+    /**
+     * Opens a file for tokenizing.
+     */
+    void open(String fileName) throws IOException, TokenizerException {
+	scanner = new Scanner();
+	scanner.open(fileName);
+	scanner.moveNext();
+	next = extractLexeme();		
+    }
+	
+    /**
+     * Returns the current token in the stream.
+     */
+    Lexeme current(){
+	return current;
+    }
+
+    /**
+     * Moves current to the next token in the stream.
+     */
+    void moveNext() throws IOException, TokenizerException {
+	if (scanner == null)
+	    throw new IOException("No open file.");
+	current = next;
+	if (next.token() != Token.EOF)
+	    next = extractLexeme();
+    }
+
+    private void consumeWhiteSpaces() throws IOException {
+	while (Character.isWhitespace(scanner.current())){
+	    scanner.moveNext();
+	}
+    }
+
+    private Lexeme extractLexeme() throws IOException, TokenizerException {
+	consumeWhiteSpaces();
+	Character ch = scanner.current();
+	if (ch == Scanner.EOF)
+	    return new Lexeme(ch, Token.EOF);
+	else if (Character.isLetter(ch))
+	    if (ch == 'a' || ch == 't'){
+		return extractDeterminer();
+	    } else if (ch == 'c' || ch == 'm') {
+		return extractNoun();
+	    } else if (ch == 'h' || ch == 's') {
+		return extractVerb();
+	    } else throw new TokenizerException("Unknown character: " + String.valueOf(ch)); 
+	else if (symbols.containsKey(ch)) {
+	    scanner.moveNext();
+	    return new Lexeme(ch, symbols.get(ch));
+	}
+	else
+	    throw new TokenizerException("Unknown character: " + String.valueOf(ch));
 	}
 
-	@Override
-	public Lexeme current() {
-		// TODO Auto-generated method stub
-		return null;
+    private Lexeme extractDeterminer() throws IOException {
+	StringBuilder strBuilder = new StringBuilder();
+	while (Character.isLetter(scanner.current())) {
+	    strBuilder.append(scanner.current());
+	    scanner.moveNext();
 	}
+	return new Lexeme(strBuilder.toString(), Token.DETERMINER);
+    }
 
-	@Override
-	public void moveNext() throws IOException, TokenizerException {
-		// TODO Auto-generated method stub
-
+    private Lexeme extractNoun() throws IOException {
+	StringBuilder strBuilder = new StringBuilder();
+	while (Character.isLetter(scanner.current())) {
+	    strBuilder.append(scanner.current());
+	    scanner.moveNext();
 	}
+	return new Lexeme(strBuilder.toString(), Token.NOUN);
+    }
 
-	@Override
-	public void close() throws IOException {
-		// TODO Auto-generated method stub
-
+    private Lexeme extractVerb() throws IOException {
+	StringBuilder strBuilder = new StringBuilder();
+	while (Character.isLetter(scanner.current())) {
+	    strBuilder.append(scanner.current());
+	    scanner.moveNext();
 	}
+	return new Lexeme(strBuilder.toString(), Token.VERB);
+    }
+
+    /**
+     * Closes the file and releases any system resources associated with it.
+     */
+    public void close() throws IOException {
+		if (scanner != null)
+			scanner.close();
+    }
 
 }
+
+
